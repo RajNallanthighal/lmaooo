7a5d2a7978d3c72a6c83cad977114036d5850e50
Raj Nallanthighal <88398020+RajNallanthighal@users.noreply.github.com>
2024-09-19 14:51:37 -0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -4,8 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Howdy, Main!
-    WOAH! THIS IS NEW!'
+    return 'Howdy, Main! WOAH! THIS IS NEW!'
 
 @app.route('/about')
 def about():
