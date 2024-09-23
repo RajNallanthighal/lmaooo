e617aeed1674844278fea3c2a67df2f467dd928d
Raj Nallanthighal <88398020+RajNallanthighal@users.noreply.github.com>
2024-09-23 17:54:31 -0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'PLEASE PLEASE PLEASE BRUHRHRHR'
+    return 'PLEASE PLEASE PLEASE IF THERES A GOD '
 
 @app.route('/about')
 def about():
